6fadb4d5af7ff7bc9f18e33ebcb57381de3379b6
El azzouzi Hassan <ezhassan.info@gmail.com>
2023-07-17 16:14:50 +0100

    updates : chakraProvider added

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 
 import styles from './page.module.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>My NEXT JS app</h1>
-    </main>
+    <ChakraProvider>
+      <main className={styles.main}>
+        <h1>My NEXT JS app</h1>
+      </main>
+    </ChakraProvider>
   )
 }
